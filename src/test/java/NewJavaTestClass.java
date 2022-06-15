bbc799fb66f0060e7981f75d32ab042fbd55129a
pinhaskonde <pinhas.konde@gmail.com>
2022-06-15 12:14:39 +0300

    libraries

--- src/test/java/NewJavaTestClass.java
+++ src/test/java/NewJavaTestClass.java
@@ -1,2 +1,14 @@
 public class NewJavaTestClass {
+
+
+
+
+
 }
+
+
+//    Write a short test script that will :
+//    1-	Click on "join us".
+//    2-	Fill in the forms, inc. some selections in the Check boxes .
+//    fill in an email/pw
+//    And submit
